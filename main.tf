5329ef6d8f8ef1d5b4555f35439dc8bd6fbb613c
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-15 17:04:27 -0400

    fix: escape the interpolation

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ resource "null_resource" "packer_build" {
         set -e
 
         # Download and install Packer binary directly
-        PACKER_VERSION="1.11.2"
-        wget -q -O /tmp/packer.zip "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
+        PACKER_VERSION="1.15.0"
+        wget -q -O /tmp/packer.zip "https://releases.hashicorp.com/packer/$${PACKER_VERSION}/packer_$${PACKER_VERSION}_linux_amd64.zip"
         unzip -o /tmp/packer.zip -d /tmp/
         chmod +x /tmp/packer
         export PATH="/tmp:$PATH"

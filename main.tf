d9c9976775096683c4241236c9e4a4a04eb230a8
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-15 16:58:24 -0400

    feat: install packer on Terraform runner inline.

--- main.tf
+++ main.tf
@@ -5,11 +5,19 @@ resource "null_resource" "packer_build" {
 
   provisioner "local-exec" {
     command = <<-EOT
+      # Install Packer
+      wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+      echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+      apt-get update -y && apt-get install -y packer
+
+      # Run Packer build
       cd ${path.module}
       packer init builds/al2023-demo/
       packer build \
         -var-file=builds/al2023-demo/variables.pkrvars.hcl \
         builds/al2023-demo/
     EOT
+
+    interpreter = ["/bin/bash", "-c"]
   }
 }
